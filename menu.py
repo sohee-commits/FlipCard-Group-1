fd105be22c792f1e84db1009de112737feb957f3
소희 <mintopiate@gmail.com>
2024-03-12 09:02:41 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,9 +1,9 @@
 from tkinter import *
-from tkinter import ttk
 from PIL import Image, ImageTk
-import cards
+import cards_js
+import cards_py
 import login 
-import about 
+import about
 
 def openMenu():    
     menu = Toplevel()
@@ -11,7 +11,7 @@ def openMenu():
     menu.geometry('1280x720')
     menu.configure(background='#fdfdfd')
 
-    o_program = Button(
+    about_program = Button(
         menu,
         text='О программе', 
         font=('Roboto', 16, 'bold'),
@@ -23,9 +23,9 @@ def openMenu():
         borderwidth=0,
         command=about.openAbout
     )
-    o_program.place(relx=.27, y=30, anchor='n');
+    about_program.place(relx=.27, y=30, anchor='n');
 
-    themes = Button(
+    topics = Button(
         menu,
         text='Темы', 
         font=('Roboto', 16, 'bold'),
@@ -37,7 +37,7 @@ def openMenu():
         borderwidth=0,
         command=None
     )
-    themes.place(relx=.5, y=30, anchor='n');
+    topics.place(relx=.5, y=30, anchor='n');
 
     logout = Button(
         menu,
@@ -53,14 +53,14 @@ def openMenu():
     )
     logout.place(relx=.73, y=30, anchor='n');
 
-    themes_text = Label(
+    topics_text = Label(
         menu,
         text='Темы', 
         font=('Roboto', 32, 'bold'),
         foreground='#0047AB',
         background='#fdfdfd'
     )
-    themes_text.place(relx=.5, y=100, anchor='n');
+    topics_text.place(relx=.5, y=100, anchor='n');
 
     url_js='assets/javascript 1.png'
     url_py='assets/python 1.png'
@@ -73,7 +73,7 @@ def openMenu():
     img_js = ImageTk.PhotoImage(img_js)
     img_py = ImageTk.PhotoImage(img_py)
 
-    py_theme = Button(
+    py_topic = Button(
         menu,
         image=img_py,
         compound=LEFT,
@@ -83,12 +83,12 @@ def openMenu():
         background='#FFFFFF',
         cursor='hand2',
         borderwidth=0,
-        command=cards.openCards
+        command=cards_py.openCards
     )
-    py_theme.image = img_py # Сохранение ссылки на изображение
-    py_theme.place(relx=.48, y=175, anchor='n');
+    py_topic.image = img_py # Сохранение ссылки на изображение
+    py_topic.place(relx=.48, y=175, anchor='n');
 
-    js_theme = Button(
+    js_topic = Button(
         menu,
         image=img_js,
         compound=LEFT,
@@ -98,9 +98,9 @@ def openMenu():
         background='#FFFFFF',
         cursor='hand2',
         borderwidth=0,
-        command=cards.openCards
+        command=cards_js.openCards
     )
-    js_theme.image = img_js # Сохранение ссылки на изображение
-    js_theme.place(relx=.5, y=225, anchor='n');
+    js_topic.image = img_js # Сохранение ссылки на изображение
+    js_topic.place(relx=.5, y=225, anchor='n');
 
     menu.mainloop()
